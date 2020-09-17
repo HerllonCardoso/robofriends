688a70fc68855f97258a9e8896d290b44fedd9a8
Herllon Cardoso <cardoso.herllon@gmail.com>
2020-09-16 22:03:42 -0300

    classname tc

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import SearchBox from './SearchBox'
 
 const App = () => {
     return(
-        <div className='tc'>
+        <div className='tc '>
             <h1>RoboFriends</h1>
             <SearchBox />
             <CardList robots={robots} />
