118eb51973307760a2c01d3b21e0c7077d9e397d
Herllon Cardoso <cardoso.herllon@gmail.com>
2020-09-16 22:03:01 -0300

    add search box layout

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
-import * as serviceWorker from './serviceWorker';
+import App from './App'
 import 'tachyons';
 import { robots } from './robots'
 
@@ -10,13 +9,9 @@ import { robots } from './robots'
 ReactDOM.render(
   <React.StrictMode>
     
-      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
-    
+      <App /> 
+
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-
-// serviceWorker.unregister();
